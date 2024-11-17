1cbe40a6d980cf03807a5aeecd12c2db1f68e843
Enriko Matheus <enrikomatheus@gmail.com>
2024-11-16 21:49:51 -0300

    Adicionar conecção banco de dados

--- ProjetoPetShop/src/Main.java
+++ ProjetoPetShop/src/Main.java
@@ -1,2 +1,10 @@
+import java.sql.Connection;
+
 public class Main {
+    public static void main(String[] args) {
+        PostgresConnection postgresConnection = new PostgresConnection();
+        Connection connection = postgresConnection.getInstance();
+
+    }
+
 }
--- ProjetoPetShop/src/PostgresConnection.java
+++ ProjetoPetShop/src/PostgresConnection.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class PostgresConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgresConnection {
+    private Connection connection;
+
+    public Connection getInstance() {
+        if (this.connection == null) {
+            connection = createConnection();
+
+        }
+        return connection;
+    }
+
+    private Connection createConnection() {
+        String url = "jdbc:postgresql://localhost:5432/petshop";
+        String user = "postgres";
+        String password = "1234";
+
+
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexão estabelecida com sucesso!");
+            return connection;
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver não encontrado no classpath!");
+            throw new RuntimeException(e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar ao banco de dados:");
+            throw new RuntimeException(e.getMessage());
+
+        }
+    }
 }
