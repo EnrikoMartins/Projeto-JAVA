6d4ef4ad45d1eb3657bfbe5768a0d2a9150d0662
Enriko Matheus <enrikomatheus@gmail.com>
2024-11-17 22:04:26 -0300

    Adicionar entidade Sistema

--- ProjetoPetShop/src/Main.java
+++ ProjetoPetShop/src/Main.java
@@ -5,6 +5,24 @@ public class Main {
         PostgresConnection postgresConnection = new PostgresConnection();
         Connection connection = postgresConnection.getInstance();
 
+        System.out.println("Bem vindo ao PetShop com Jesus");
+        System.out.println();
+
+        System.out.println("Menu: ");
+        System.out.println();
+
+        System.out.println("1 - Entrar como Gerente");
+        System.out.println("2 - Entrar como Cliente");
+        System.out.println("3 - Sair");
+        // gerente
+        System.out.println("1 - Inscrever Adotante/ Voluntario/ Animal");
+        System.out.println("2 - Apagar Adotante/ Voluntario/ Animal");
+        System.out.println("3 - Sair");
+        // cliente
+        System.out.println("1 - Adotar animal");
+        System.out.println("2 - Benzer animal");
+        System.out.println("3 - Sair");
+
     }
 
 }
---
+++ ProjetoPetShop/src/SistemaPetshop.java
@@ -0,0 +1,5 @@
+public class SistemaPetshop {
+    public void MenuPrincipal() {
+
+    }
+}
