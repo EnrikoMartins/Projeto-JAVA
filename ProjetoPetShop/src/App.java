d264210d06d37dabfa2397f9512b4c7958c40c93
FelipinasBR77 <139183168+FelipinasBR77@users.noreply.github.com>
2024-11-30 13:50:33 -0300

    App (tirou a parte da lista de voluntários)

--- ProjetoPetShop/src/App.java
+++ ProjetoPetShop/src/App.java
@@ -3,11 +3,9 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        List <Pessoa> voluntarios = new ArrayList<>();
-
         System.out.println("Bem vindo ao Petshop com Jesus!\n");
 
         SistemaPetshop sistemaPetshop = new SistemaPetshop();
-        sistemaPetshop.logicaSistema(sistemaPetshop.menuPrincipal(), voluntarios);
+        sistemaPetshop.logicaSistema(sistemaPetshop.menuPrincipal());
     }
 }
